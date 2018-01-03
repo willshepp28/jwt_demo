97ef5676a386cc315f352ee17cc335719e7d3a90
willshepp28 <willsheppard28@outlook.com>
2018-01-02 22:42:20 -0500

    included protected route

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express'),
+    jwt = require('jsonwebtoken'),
     application = express();
 
 
@@ -8,6 +9,45 @@ application.get('/api', ( request, response) => {
     response.json({ message: 'this is the api'});
 })
 
+
+
+application.post('/api/login', (request, response) => {
+
+    // fake user
+    const user = { id: 3 };
+
+    const token = jwt.sign({ user }, 'my_secret_key');
+    response.json({ token: token})
+});
+
+
+application.get('/api/protected', ensureToken, (request, response) => {
+
+    jwt.verify(request.token, 'my_secret_key', ( err, data) => {
+        if (err) {
+            respones.sendStatus(403);
+        } else {
+            response.json({ 
+                message: 'this is protected',
+                data: data
+            });
+        }
+    })
+    response.json({ message: 'this is a protected route'});
+});
+
+
+function ensureToken(request, response, next){
+    const bearerHeader = request.headers['authorization']
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(" ");
+        const bearerToken = bearer[1];
+        request.token = bearerToken;
+        next();
+    } else {
+        response.sendStatus(403);
+    }
+}
 
 
 application.listen(3000, () => {
